3fe631db088ed69548a8b81747866cb943bac777
brendonparker <brendon.parker@rfsmart.com>
2023-04-19 15:18:40 -0400

    Add stub for CDK

---
+++ IAC/Program.cs
@@ -0,0 +1,20 @@
+﻿using Amazon.CDK;
+
+namespace SFF.Infrastructure;
+
+sealed class Program
+{
+    public static void Main(string[] args)
+    {
+        var app = new App();
+        new ServerlessFeatureFlagsStack(app, "ServerlessFeatureFlagsStack", new StackProps
+        {
+            Env = new Amazon.CDK.Environment
+            {
+                Account = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_ACCOUNT"),
+                Region = System.Environment.GetEnvironmentVariable("CDK_DEFAULT_REGION"),
+            }
+        });
+        app.Synth();
+    }
+}
